a4b436c54f1ebe37e1951cc7f913c27590ccc7b1
Pierre GREBERT <pierrot62160@hotmail.fr>
2025-02-07 12:07:24 +0100

    fix migrations and wait test

--- migrations/Version20250206205527.php
+++ migrations/Version20250206205527.php
@@ -7,19 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250206205527 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return '[#8] Create user table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, email VARCHAR(180) NOT NULL, password VARCHAR(20) NOT NULL, roles JSON NOT NULL, username VARCHAR(20) NOT NULL, biography VARCHAR(200) DEFAULT NULL, birthdate TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, profile_img_path VARCHAR(255) NOT NULL, background_img_path VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_IDENTIFIER_EMAIL ON "user" (email)');
         $this->addSql('COMMENT ON COLUMN "user".birthdate IS \'(DC2Type:datetime_immutable)\'');
@@ -27,7 +23,6 @@ final class Version20250206205527 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP TABLE "user"');
     }
