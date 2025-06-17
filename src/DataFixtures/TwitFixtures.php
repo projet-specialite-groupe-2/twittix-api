3194412c1f6ee1fda6e4e2e88f3b7e8e52d57994
Sangryos <93972396+Sangryos@users.noreply.github.com>
2025-06-17 16:13:31 +0200

    Add fixtures (#73)

--- src/DataFixtures/TwitFixtures.php
+++ src/DataFixtures/TwitFixtures.php
@@ -9,6 +9,7 @@ use App\Enum\TwitStatus;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class TwitFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -21,13 +22,11 @@ class TwitFixtures extends Fixture implements DependentFixtureInterface
         $twit->setContent('Hello world!');
         $twit->setStatus(TwitStatus::PUBLISHED);
         $twit->setAuthor($this->getReference(UserFixtures::USER_REFERENCE, User::class));
-
         $manager->persist($twit);
 
         $like = new Like();
         $like->setTwit($twit);
         $like->setAuthor($this->getReference(UserFixtures::USER_REFERENCE2, User::class));
-
         $manager->persist($like);
 
         // Create a twit archived by the first user
@@ -35,11 +34,21 @@ class TwitFixtures extends Fixture implements DependentFixtureInterface
         $twit->setContent('Goodbye world!');
         $twit->setStatus(TwitStatus::DELETED);
         $twit->setAuthor($this->getReference(UserFixtures::USER_REFERENCE, User::class));
-
         $manager->persist($twit);
-        $manager->flush();
 
         $this->addReference(self::TWIT_REFERENCE, $twit);
+
+        $faker = Factory::create();
+
+        for ($i = 0; $i < 20; $i++) {
+            $twit = new Twit();
+            $twit->setContent($faker->sentence($nbWords = 16, $variableNbWords = true));
+            $twit->setStatus(TwitStatus::PUBLISHED);
+            $twit->setAuthor($this->getReference($i%2 === 0 ? UserFixtures::USER_REFERENCE : UserFixtures::USER_REFERENCE2, User::class));
+            $manager->persist($twit);
+        }
+
+        $manager->flush();
     }
 
     public function getDependencies(): array
